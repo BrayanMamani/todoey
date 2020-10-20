d400739ee8c7d3cbe59ba25e59e2be112bd9fae8
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-10-20 16:03:14 -0400

    Mark task done

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -11,7 +11,12 @@ class _TaskTileState extends State<TaskTile> {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task,'),
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
       trailing: TaskCheckbox(
         checkboxState: isChecked,
         toggleCheckboxState: (bool checkboxState) {
