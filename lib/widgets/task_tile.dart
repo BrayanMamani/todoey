a061c3972bfb61dd0730c087b0ce3df8e6e2ba6a
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-10-20 14:27:09 -0400

    Use local state

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -5,9 +5,29 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('This is a task,'),
-      trailing: Checkbox(
-        value: false,
-      ),
+      trailing: TaskCheckbox(),
+    );
+  }
+}
+
+class TaskCheckbox extends StatefulWidget {
+  @override
+  _TaskCheckboxState createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool checkboxState = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: (newValue) {
+        setState(() {
+          checkboxState = newValue;
+        });
+      },
     );
   }
 }
